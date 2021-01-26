eef8bd1c8479ae22df9b1f86975febdb7d7cd117
Kasfil Aziz <kasf@tuta.io>
2021-01-26 07:16:40 +0700

    going to wrap into single class

--- authenticate.py
+++ authenticate.py
@@ -6,54 +6,85 @@ Functions:
     _check_credential
     authenticate
 """
+import os
 import json
+from typing import Dict
 from pathlib import Path
 from google.oauth2.credentials import Credentials
+from google.auth.exceptions import RefreshError
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+class BetterGmailChecker:
+    # public properties
+    label_id: str
+    api_response: Dict
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.labels']
-CLIENT_SECRETS_PATH = Path('client_secrets.json').resolve()
-CREDENTIAL_PATH = Path('credentials.json').resolve()
+    # Private properties
+    _credential: Credentials = None
+    _scopes: list[str] = ['https://www.googleapis.com/auth/gmail.labels']
+    _dir: str = Path(__file__).resolve().parent
+    _client_secrets_path = Path(_dir, 'client_secrets.json').resolve()
+    _credential_path: Path = Path(_dir, 'credentials.json').resolve()
 
-def _parse_credential():
-    creds = Credentials.from_authorized_user_file(CREDENTIAL_PATH)
-    print(creds.expired)
-    print(creds.refresh_token)
-    if creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-        return None
+    def __init__(self, label_id: str = 'INBOX'):
+        # set default label to INBOX
+        self.label_id = label_id
 
-    return creds
+        # Force authenticate if no valid credential
+        while self._credential is None:
+            if self._credential_path.is_file():
+                self._parse_credential()
+            else:
+                self._authenticate()
 
-def _authenticate():
-    """Authenticate while nothing credential file"""
-    # Creating object
-    flow = InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRETS_PATH,
-        scopes = SCOPES
-    )
-    # call authentication
-    credential = flow.run_local_server()
-    # prepare credential file
-    cred_file = open(CREDENTIAL_PATH, 'w')
-    # write authentication response to credential file
-    cred_file.write(credential.to_json())
+        # execute API call
 
-def get_credential() -> Credentials:
-    """Get credential for retreive API"""
-    creds = None
+    def _write_credentials(self, credential: str) -> None:
+        """Write given credential json to file
 
-    while creds is None:
-        if CREDENTIAL_PATH.is_file():
-            creds = _parse_credential()
+        Parameter:
+            credential_json -> str
+        Return:
+            void
+        """
+        credential_file = open(self._credential_path, 'w')
+        credential_file.write(credential)
+
+    def _parse_credential(self) -> None:
+        """Parse credential from credential file
+
+        Return:
+            Credentials
+        """
+        # read credential file
+        credential = Credentials.from_authorized_user_file(self._credential_path)
+        # check if credential is expired
+        if not credential.expired:
+            self._credential = credential
         else:
-            _authenticate()
+            try:
+                credential.refresh(Request())
+                self._write_credentials(credential.to_json())
+            except RefreshError:
+                os.remove(self._credential_path)
 
-    return creds
+    def _authenticate(self) -> None:
+        """Authenticate while nothing credential file"""
+        # Creating object
+        flow = InstalledAppFlow.from_client_secrets_file(
+            self._client_secrets_path,
+            scopes = self._scopes
+        )
+        # call authentication
+        credential = flow.run_local_server()
+        # write authentication response to credential file
+        self._write_credentials(credential.to_json())
 
-gmail = build('gmail', 'v1', credentials=get_credential())
-response = gmail.users().labels().get(userId = 'me', id = 'INBOX').execute()
-print(json.dumps(response, sort_keys=True, indent=2))
+    def _call_api(self):
+        # build service
+        gmail = build('gmail', 'v1', credentials = self._credential)
+        # get API response
+        response = gmail.users().labels().get(userId = 'me', id = self.label_id).execute() # pylint: disable=no-member
+        self.api_response = json.loads(response)
